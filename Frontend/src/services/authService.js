4b8d4d7dac16feed2896c13a32fb0b927e07d4f4
Raju8115 <117435076+Raju8115@users.noreply.github.com>
2025-11-25 11:02:36 +0530

    Update API_URL to production endpoint

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1';
+const API_URL = 'https://solution-config.onrender.com/api/v1';
 
 class AuthService {
   async login() {
