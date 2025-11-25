a44289a225b1ae5e64f90b94c64eb78364151d16
Raju8115 <117435076+Raju8115@users.noreply.github.com>
2025-11-25 11:02:18 +0530

    Change API base URL to production endpoint

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://solution-config.onrender.com/api/v1',
   withCredentials: true,  // IMPORTANT: Send cookies with every request
   headers: {
     'Content-Type': 'application/json',
