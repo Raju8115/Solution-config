18ee323f9246d564fa0c4a882562623abf60b6b7
Raju8115 <117435076+Raju8115@users.noreply.github.com>
2025-11-25 11:14:17 +0530

    Update main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -38,7 +38,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000", 
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://solution-config-1.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
